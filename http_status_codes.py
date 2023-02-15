f9271d2d720cfdcd40d0d2003e6a0b4a7dc81928
Noertri <trinuruns@gmail.com>
2023-02-15 11:38:53 +0700

    modifikasi kecil

--- http_status_codes.py
+++ http_status_codes.py
@@ -17,8 +17,8 @@ class HTTPStatusCodes:
     CODE_100_CONTINUE: int = 100
     """100 Continue.
     
-    The server has received the request headers and the client should proceed to send the request body (in the case of a request for which a
-    body needs to be sent; for example, a POST request). Sending a large request body to a server after a request has been rejected for
+    The server has received the request headers and the client should proceed to send the request body (in the case of a request
+    for which a body needs to be sent; for example, a POST request). Sending a large request body to a server after a request has been rejected for
     inappropriate headers would be inefficient. To have a server check the request's headers, a client must send Expect: 100-continue as a
     header in its initial request and receive a 100 Continue status code in response before sending the body. If the client receives an
     error code such as 403 (Forbidden) or 405 (Method Not Allowed) then it should not send the request's body. The response 417 Expectation
